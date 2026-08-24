8ee82f923ae17ca91ec059696e3f7a1467ce3471
PharmZanga <zangamusakuzi@gmail.com>
2026-08-24 09:28:15 +0200

    Add cleaned 2025 tracer reporting history

--- src/tracerFacilityData.js
+++ src/tracerFacilityData.js
@@ -1,9 +1,10 @@
 import { tracerReportingPeriods as tracerFacilityData2024 } from "./tracerFacilityData2024.js";
+import { tracerReportingPeriods as tracerFacilityData2025 } from "./tracerFacilityData2025.js";
 import { tracerReportingPeriods as tracerFacilityDataJanFeb } from "./tracerFacilityDataJanFeb.js";
 import { tracerReportingPeriods as tracerFacilityDataMarApr } from "./tracerFacilityDataMarApr.js";
 import { tracerReportingPeriods as tracerFacilityDataMayJun } from "./tracerFacilityDataMayJun.js";
 import { tracerReportingPeriods as tracerFacilityDataJul } from "./tracerFacilityDataJul.js";
 
-export const tracerReportingPeriods = [...tracerFacilityData2024, ...tracerFacilityDataJanFeb, ...tracerFacilityDataMarApr, ...tracerFacilityDataMayJun, ...tracerFacilityDataJul];
+export const tracerReportingPeriods = [...tracerFacilityData2024, ...tracerFacilityData2025, ...tracerFacilityDataJanFeb, ...tracerFacilityDataMarApr, ...tracerFacilityDataMayJun, ...tracerFacilityDataJul];
 
 export const tracerFacilityData = tracerReportingPeriods.at(-1);
